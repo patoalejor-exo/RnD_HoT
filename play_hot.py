c701e7c8c9e2b51e079746666a93d16d613e2802
patoalejor <pato@exosystems.io>
2024-05-09 14:57:00 +0900

    feat: fn to call python using a loop

---
+++ play_hot.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+from glob import glob 
+
+# Path to the virtual environment
+venv_path = ".venv"
+
+# Path to the Python executable inside the virtual environment
+# Adjust 'bin' to 'Scripts' if you are on Windows
+python_executable = os.path.join(venv_path, 'Scripts', 'python.exe')
+
+def process_videos_hot():
+  all_videos = glob('./demo/video/finn_walk*v15_cam_0*.mp4')
+  print(all_videos)
+
+  # Loop to run a subprocess 10 times
+  for each_video in all_videos:
+    
+    title = each_video.split(os.sep)[-1]
+    print(f'Running HoT for {title}')
+
+    # Define the command to be run
+    command = [python_executable, 'demo/vis.py', '--video', title]
+
+    # Run the command
+    result = subprocess.run(command, capture_output=True, text=True)
+   
+    # Print the output
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+
+if __name__ == '__main__':
+  process_videos_hot()
